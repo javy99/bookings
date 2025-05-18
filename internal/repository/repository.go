2378670e9d3f3f172632eaea24fcf7bcd7ecfc06
javy99 <kosimov.javlonbek@bk.ru>
2025-05-18 20:33:33 +0200

    Inserting Room om Restrictions

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -5,5 +5,6 @@ import "github.com/javy99/bookings/internal/models"
 type DatabaseRepo interface {
 	AllUsers() bool
 
-	InsertReservation(res *models.Reservation) error
+	InsertReservation(res models.Reservation) (int, error)
+	InsertRoomRestriction(res models.RoomRestriction) error
 }
