f2bad4ebe1b8eb372d0cde369128b85718cf2d43
javy99 <kosimov.javlonbek@bk.ru>
2025-05-21 19:59:30 +0200

    Connecting seararch availability to the make reservation page

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/go-chi/chi"
 	"github.com/javy99/bookings/internal/config"
 	"github.com/javy99/bookings/internal/driver"
 	"github.com/javy99/bookings/internal/forms"
@@ -248,4 +249,23 @@ func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) 
 	render.Template(w, r, "reservation-summary.page.tmpl", &models.TemplateData{
 		Data: data,
 	})
+}
+
+func (m *Repository) ChooseRoom(w http.ResponseWriter, r *http.Request) {
+	roomID, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+
+	res, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
+	if !ok {
+		helpers.ServerError(w, err)
+		return
+	}
+
+	res.RoomID = roomID
+	m.App.Session.Put(r.Context(), "reservation", res)
+
+	http.Redirect(w, r, "/make-reservation", http.StatusSeeOther)
 }
