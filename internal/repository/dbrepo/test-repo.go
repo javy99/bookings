4e1aacbf3b714589b0c18dc24f2d064ca803c488
javy99 <kosimov.javlonbek@bk.ru>
2025-05-25 15:06:27 +0200

    Improving our tests by handling multiple cases

--- internal/repository/dbrepo/test-repo.go
+++ internal/repository/dbrepo/test-repo.go
@@ -1,6 +1,7 @@
 package dbrepo
 
 import (
+	"errors"
 	"time"
 
 	"github.com/javy99/bookings/internal/models"
@@ -35,6 +36,9 @@ func (m *testDBRepo) SearchAvailabilityForAllRooms(start, end time.Time) ([]mode
 // GetRoomById gets a room by id
 func (m *testDBRepo) GetRoomById(id int) (models.Room, error) {
 	var room models.Room
+	if id > 2 {
+		return room, errors.New("Some error")
+	}
 
 	return room, nil
 }
